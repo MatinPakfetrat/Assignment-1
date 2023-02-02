5c5790e56120d940c0108ce226e708185ef50b23
Matin <matinpaki83@gmail.com>
2023-02-01 19:07:28 -0500

    Game story done

--- App.py
+++ App.py
@@ -3,3 +3,16 @@ This is the only module that interacts with the player. It starts off with the p
 then prints the story and the roles and lets the player choose their role,
 finally starts the challenges and prints a win or loss game over message.
 '''
+import Game
+
+start = input("Press enter to start the game.")  #Once the player presses Enter the game will start
+
+print("The Dragon Slayer")
+
+print('''Welcome slayer!\nWe've been waiting so long for you! Thank you so much for coming!
+You see that castle up there? That is where the dragon is.
+This village has not seen peace since it took over it!
+If you kill the dragon, peace will return, and we will pay you handsomely!
+But it will not be easy! There are three challenges ahead of you!''')
+
+
